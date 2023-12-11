0b5b1a4cd657db8106aafa4fb0867dd885a02b44
LOUDINI SOUAD <souad.llddnn@gmail.com>
2023-12-11 17:42:23 +0100

    update orderbook class

--- main.py
+++ main.py
@@ -42,15 +42,28 @@ class OrderBook:
         for ask in self.asks:
             print(f"Price: {ask[0]}, Quantity: {ask[1]}")
 
+order_book = OrderBook()
+
 def on_message(ws, message):
     trade_data = json.loads(message)
-    print(f"Trade Event:")
-    print(f"Symbol: {trade_data['s']}")
-    print(f"Price: {trade_data['p']}")
-    print(f"Quantity: {trade_data['q']}")
-    print(f"Buyer Maker: {trade_data['m']}")
-    print(f"Trade Time: {trade_data['T']}")
-    print("")
+
+   
+    if 'bids' in trade_data and 'asks' in trade_data:
+        for bid in trade_data['bids']:
+            order_book.add_bid(float(bid[0]), float(bid[1]))  
+        for ask in trade_data['asks']:
+            order_book.add_ask(float(ask[0]), float(ask[1]))  
+        order_book.print_order_book() 
+    else:
+      
+        print(f"Trade Event:")
+        print(f"Symbol: {trade_data['s']}")
+        print(f"Price: {trade_data['p']}")
+        print(f"Quantity: {trade_data['q']}")
+        print(f"Buyer Maker: {trade_data['m']}")
+        print(f"Trade Time: {trade_data['T']}")
+        print("")
+
 
 def on_error(ws, error):
     print(f"Error: {error}")
@@ -73,7 +86,6 @@ if __name__ == "__main__":
     ws = websocket.WebSocketApp(endpoint, on_message=on_message, on_error=on_error, on_close=on_close)
     ws.on_open = on_open
 
-    
     websocket_thread = threading.Thread(target=run_websocket)
     websocket_thread.start()
 
@@ -82,13 +94,12 @@ if __name__ == "__main__":
     while time.time() - start_time < event_timeout:
         time.sleep(1)
 
-    
     print(f"Stopping after {event_timeout} seconds")
     ws.close()
 
     order_book = OrderBook()
     order_book.add_bid(180, 12)
-    order_book.add_ask(170, 19)
+    order_book.add_ask(166, 15)
     order_book.print_order_book()
 
     matched_quantity = order_book.match_orders()
